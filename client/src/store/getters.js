36ebc237636ca01f79c848043bd6100297272e14
eotkd4791 <eotkd4791@gmail.com>
2020-06-15 22:56:03 +0900

    Add Object for LeftLink.vue

--- client/src/store/getters.js
+++ client/src/store/getters.js
@@ -1,5 +1,42 @@
-
-
 export default {
-
+  getCustomerLinks() {
+    return [
+      {
+        routePath: '/',
+        name: '고객센터'
+      },
+      {
+        routePath: '/',
+        name: '배송가이드'
+      },
+      {
+        routePath: '/',
+        name: '공지사항'
+      },
+      {
+        routePath: '/',
+        name: '자주묻는질문'
+      },
+      {
+        routePath: '/',
+        name: '이용약관'
+      },
+    ];
+  },
+  getUserInfoLinks() {
+    return [
+      {
+        routePath: '/',
+        name: '내 정보 관리'
+      },
+      {
+        routePath: '/',
+        name: '주문 및 반품 내역'
+      },
+      {
+        routePath: '/',
+        name: '포인트'
+      },
+    ];
+  },
 }
