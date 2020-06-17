fc695e1668136502882928d91fb5f6cd5686271f
eotkd4791 <eotkd4791@gmail.com>
2020-06-18 04:25:23 +0900

    Update LogiGuide v-once, styling

--- client/src/views/LogiGuide.vue
+++ client/src/views/LogiGuide.vue
@@ -1,33 +1,57 @@
 <template>
-  <section class="logi-guide">
+  <section class="logi-guide" v-once>
     <header class="logi-guide__header">배송가이드</header>
-    <info-list></info-list>
+    <ul class="info-list__ul">
+      <info-list v-for="item in listItems" :key="item.title" :eachOfListItems="item" />
+    </ul>
   </section>
 </template>
 
 <script>
 import InfoList from '@/components/common/InfoList.vue';
+import customerService from '@/static/customerService.js';
+
 export default {
   components: {
     InfoList,
   },
   data() {
     return {
-      
+      listItems: [],
     };
+  },
+  created() {
+    this.listItems = customerService.logiGuide;
   }
 }
 </script>
 
 <style scoped>
 .logi-guide {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
   width: 840px;
-  height: 100%;
+  min-height: 532px;
 }
 .logi-guide__header {
   font-size: 20px;
   margin: 0 0 36px;
 }
+.info-list__ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+}
 
-
+.logi-list__indent {
+  list-style-type: symbols('▶︎');
+  text-indent: 20px;
+}
+.logi-guide__none {
+  list-style-type: none;
+}
 </style>
