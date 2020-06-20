10d3e4a9668009074d6c32fe9cebe1a8128dde76
eotkd4791 <eotkd4791@gmail.com>
2020-06-21 02:33:07 +0900

    Create MyUserInfo.vue

---
+++ client/src/views/MyUserInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="user-info">
+    <left-link />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import LeftLink from '@/components/common/LeftLink.vue';
+
+export default {
+  components: {
+    LeftLink
+  },
+}
+</script>
+
+<style scoped>
+.user-info {
+  display:flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  max-width: 1100px;
+  min-height: 532px;
+  margin: 150px auto 0;
+  z-index: 1001;
+}
+</style>
