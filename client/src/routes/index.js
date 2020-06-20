e34b80e8c945455acdf3dbda093340b1f33dd5af
eotkd4791 <eotkd4791@gmail.com>
2020-06-21 02:31:52 +0900

    Add CustomerService, UserInfo Object In Routes Property & Update  Path Of Components

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,54 +1,103 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Notice from '../components/Notice.vue';
 
 
-import FaqList from '../views/FaqList.vue';
-import LogiGuide from '../views/LogiGuide.vue';
+import CustomerService from '@/views/CustomerService.vue';
+import CustomerServiceMain from '@/views/CustomerServiceMain.vue';
+import LogiGuide from '@/views/LogiGuide.vue';
+import Notice from '@/views/NoticeView.vue';
+import FaqList from '@/views/FaqList.vue';
+import Terms from '@/views/Terms.vue';
+import PrivateInfoPolicy from '@/views/PrivateInfoPolicy.vue';
 
-import Introduction from '../views/Introduction.vue';
-import CustomerService from '../views/CustomerService.vue';
-import Reviews from '../views/Reviews.vue';
-import ProductView from '../views/ProductView.vue';
-import MyPoint from '../views/MyPoint.vue';
-import LoginView from '../views/LoginView.vue';
-import SignUpView from '../views/SignUpView.vue';
-import WishList from '../views/WishList.vue';
-import CartItems from '../views/CartItems.vue';
 
-import MainView from '../views/MainView.vue';
-import Terms from '../views/Terms.vue';
-import PrivateInfo from '../views/PrivateInfo.vue';
+import MyUserInfo from '@/views/MyUserInfo.vue';
+import UserInfoEdit from '@/views/UserInfoEdit.vue';
+import TotalOrderRefund from '@/views/TotalOrderRefund.vue';
+import PointUsage from '@/views/PointUsage.vue';
+
+
+import LoginView from '@/views/LoginView.vue';
+import SignUpView from '@/views/SignUpView.vue';
+
+
+import Introduction from '@/views/Introduction.vue';
+import Reviews from '@/views/Reviews.vue';
+import ProductView from '@/views/ProductView.vue';
+import WishList from '@/views/WishList.vue';
+import CartItems from '@/views/CartItems.vue';
+
+import MainView from '@/views/MainView.vue';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   mode: "history",
-  routes: [{
+  routes: [
+    {
       path: '/',
-      component: MainView,
       name: 'main',
-    },
-    {
-      path: '/notice',
-      component: Notice,
-      name: 'notice'
-    },
-    {
-      path: '/faq',
-      component: FaqList,
-      name: 'faq'
+      component: MainView,
     },
     {
       path: '/customerservice',
+      redirect: '/customerservice/main',
       component: CustomerService,
-      name: 'customerService'
+      children: [
+        {
+          path: '/customerservice/main',
+          name: 'customerService',
+          component: CustomerServiceMain,
+        },
+        {
+          path: '/customerservice/logi_guide',
+          name: 'logiGuide',
+          component: LogiGuide,
+        },
+        {
+          path: '/customerservice/notice',
+          name: 'notice',
+          component: Notice,
+        },
+        {
+          path: '/customerservice/faq',
+          name: 'faqList',
+          component: FaqList,
+        },
+        {
+          path: '/customerservice/terms',
+          name: 'terms',
+          component: Terms,
+        },
+        {
+          path: '/customerservice/private_info_policy',
+          name: 'privateInfoPolicy',
+          component: PrivateInfoPolicy,
+        },
+      ]
     },
     {
-      path: '/logi_guide',
-      component: LogiGuide,
-      name: 'logiGuide'
+      path: '/user',
+      alias: '/user/edit',
+      component: MyUserInfo,
+      children: [
+        {
+          path: '/user/edit',
+          name: 'UserInfoEdit',
+          component: UserInfoEdit
+        },
+        {
+          path: '/user/point',
+          name: 'PointUsage',
+          component: PointUsage
+        },
+        {
+          path: 'user/total_order_refund',
+          name: 'TotalOrderRefund',
+          component: TotalOrderRefund
+        }
+      ]
     },
     {
       path: '/introduction',
@@ -64,19 +113,6 @@ export default new VueRouter({
       path: '/products',
       component: ProductView,
       name: 'products'
-    },
-    {
-      path: '/terms',
-      component: Terms,
-      name: 'terms'
-    },
-    {
-      path: '/privateinfo',
-      component: PrivateInfo,
-    },
-    {
-      path: '/mypoint',
-      component: MyPoint,
     },
     {
       path: '/login',
@@ -99,14 +135,15 @@ export default new VueRouter({
       name: 'cartItems'
     },
   ],
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return {
-        x: 0,
-        y: 0
-      };
-    }
-  }
+
+  // scrollBehavior(to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition;
+  //   } else {
+  //     return {
+  //       x: 0,
+  //       y: 0
+  //     };
+  //   }
+  // }
 });
