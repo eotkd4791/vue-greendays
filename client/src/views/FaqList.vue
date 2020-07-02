f611187be8c5700545d381a7878a7c95324f1aad
eotkd4791 <eotkd4791@gmail.com>
2020-07-02 23:32:38 +0900

    Adjust Prettier

--- client/src/views/FaqList.vue
+++ client/src/views/FaqList.vue
@@ -1,42 +1,42 @@
 <template>
-  <section class="faq-list" v-once>
-    <header class="faq-list__header">자주묻는질문</header>
-    <ul class="faq-list__ul">
-      <info-list v-for="item in listItems" :key="item.title" :eachOfListItems="item" />
-    </ul>
-  </section>
+	<section class="faq-list" v-once>
+		<header class="faq-list__header">자주묻는질문</header>
+		<ul class="faq-list__ul">
+			<info-list v-for="item in listItems" :key="item.title" :eachOfListItems="item" />
+		</ul>
+	</section>
 </template>
 
 <script>
 import InfoList from '@/components/common/InfoList.vue';
-import customerServiceList from '@/mixins/customerServiceList.js'
+import customerServiceList from '@/mixins/customerServiceList.js';
 
 export default {
-  components: {
-    InfoList,
-  },
-  mixins: [customerServiceList]
-}
+	components: {
+		InfoList,
+	},
+	mixins: [customerServiceList],
+};
 </script>
 
 <style scoped>
 .faq-list {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  width: 840px;
-  min-height: 532px;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	width: 840px;
+	min-height: 532px;
 }
 .faq-list__header {
-  font-size: 20px;
-  margin: 0 0 36px;
+	font-size: 20px;
+	margin: 0 0 36px;
 }
 .faq-list__ul {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
 }
 </style>
