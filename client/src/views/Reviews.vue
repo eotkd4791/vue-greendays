56ae2b88fb546cdb757d0ddde2e2cefa7f4125e6
eotkd4791 <eotkd4791@gmail.com>
2020-07-02 23:36:36 +0900

    Adjust Prettier

--- client/src/views/Reviews.vue
+++ client/src/views/Reviews.vue
@@ -6,24 +6,25 @@
 </template>
 
 <script>
-import InspectionCenter from '@/components/InspectionCenter.vue';
-import UserReviews from '@/components/UserReviews.vue';
+import InspectionCenter from "@/components/InspectionCenter.vue";
+import UserReviews from "@/components/UserReviews.vue";
 
 export default {
   components: {
     InspectionCenter,
     UserReviews
-  },
-}
+  }
+};
 </script>
 
 <style scoped>
 .reviews {
-  display:flex;
+  display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
   max-width: 1100px;
+  width: 960px;
   margin: 150px auto 0;
 }
 </style>
