65b6af696af5b4547d8c84af7ec530261ff23786
eotkd4791 <eotkd4791@gmail.com>
2020-07-04 01:32:21 +0900

    Make HTML Tags More Semantic & Adjust BEM To ClassName & CSS

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,103 +1,129 @@
 <template>
-  <footer class="container">
-    <section class="info-greendays">
-      <div>
-        <router-link to="/notice">공지사항</router-link>|
-        <router-link to="/faq">자주묻는질문</router-link>|  
-        <router-link to="/terms">이용약관</router-link>|  
-        <router-link to="/privateinfo">개인정보취급방침</router-link>|
-        <router-link to="/notice">사업자정보확인</router-link>
-      </div>
-      <p>
-        <span>(주)그린데이즈</span>|
-        <span>대표이사 유대상</span>|
-        <span>사업자등록번호 123-45-11223</span>|
-        <span>통신판매업신고 2020-인천학익-12345</span>
-      </p>
-      <p>
-        <span>eotkd4791@gmail.com</span>|
-        <span>032-866-XXXX</span>|
-        <span>인천광역시 미추홀구</span>|
-        <span>개인정보관리책임자 유대상</span>
-      </p>      
-      <p>Copyright © 2020 GreenDays Inc. All rights reserved.</p>
-    </section>
-    <section class="menu-footer">
-      <div class="cs-menu">
-        <p>CUSTOMER SERVICE</p>
-        <router-link to="/customer">무엇을 도와드릴까요?</router-link>
-        <router-link to="/faq">주문 & 배송</router-link>
-        <router-link to="/faq">결제 & 가격</router-link>
-        <router-link to="/faq">반품 & 환불</router-link>
-      </div>
-      <div class="about-gdays">
-        <p>ABOUT GREENDAYS</p>
-        <router-link to="/introduction">그린데이즈 소개</router-link>
-      </div>
-    </section>
-  </footer>
+	<footer class="about">
+		<section class="about__section">
+			<ul class="about__ul about__ul--title">
+				<router-link tag="li" class="about__router-li" to="/customerservice/notice">공지사항</router-link>
+				<span class="about__divider">|</span>
+				<router-link tag="li" class="about__router-li" to="/customerservice/faq">자주묻는질문</router-link>
+				<span class="about__divider">|</span>
+				<router-link tag="li" class="about__router-li" to="/customerservice/terms">이용약관</router-link>
+				<span class="about__divider">|</span>
+				<router-link
+					tag="li"
+					class="about__router-li"
+					to="/customerservice/private_info_policy"
+				>개인정보취급방침</router-link>
+				<span class="about__divider">|</span>
+				<router-link tag="li" class="about__router-li" to="/introduction">사업자정보확인</router-link>
+			</ul>
+			<ul class="about__ul about__ul--mid">
+				<li class="about__li">(주)그린데이즈</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">대표이사 유대상</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">사업자등록번호 123-45-11223</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">통신판매업신고 2020-인천학익-12345</li>
+			</ul>
+			<ul class="about__ul">
+				<li class="about__li">eotkd4791@gmail.com</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">032-866-XXXX</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">인천광역시 미추홀구</li>
+				<span class="about__divider">|</span>
+				<li class="about__li">개인정보관리책임자 유대상</li>
+			</ul>
+			<p class="about__p">Copyright © 2020 GreenDays Inc. All rights reserved.</p>
+		</section>
+		<aside class="about__aside">
+			<ul class="about__ul about__ul--left">
+				<li class="about__li about__li--title">CUSTOMER SERVICE</li>
+				<router-link tag="li" class="about__router-li" to="/customerservice">무엇을 도와드릴까요?</router-link>
+				<router-link tag="li" class="about__router-li" to="/customerservice/logi_guide">주문 & 배송</router-link>
+				<router-link tag="li" class="about__router-li" to="/customerservice/faq">결제 & 가격</router-link>
+				<router-link tag="li" class="about__router-li" to="/customerservice/faq">반품 & 환불</router-link>
+			</ul>
+			<ul class="about__ul">
+				<li class="about__li about__li--title">ABOUT GREENDAYS</li>
+				<router-link tag="li" class="about__router-li" to="/introduction">그린데이즈 소개</router-link>
+			</ul>
+		</aside>
+	</footer>
 </template>
 
-<script>
-export default {}
-</script>
 
 <style scoped>
-p {
-  margin: 0 0 14px 0;
-}
-/* section 1 */
-.container {
-  width:100%;
-  max-width: 1100px;
-  height: 255px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  margin: 0 auto;
-  padding: 50px 0;
-}
-a {
-  text-decoration: none;
-  color: #767676;
-  font-size: 13px;
-  padding: 0 5px;
-}
-.info-greendays {
-  font-size: 12px;
-  font-weight: 500;
-  color: #B8B8B8;
-}
-.info-greendays p:nth-child(4) {
-  padding: 10px 5px;
-}
-.info-greendays div {
-  padding: 0 0 20px 0;
-}
-.info-greendays p span {
-  padding: 0 5px;
+.about {
+	display: flex;
+	justify-content: space-between;
+	align-self: flex-start;
+	max-width: 1100px;
+	width: 100%;
+	height: 260px;
+	margin: 0 auto;
+	padding: 44px 0 0 0;
+	font-size: 12px;
+	color: #b5b5b5;
 }
 
-/* section 2 */
-.menu-footer div p{
-  color: #464646;
-  font-size: 14px;
+.about__section {
+	display: flex;
+	flex-direction: column;
+	width: 50%;
 }
-.cs-menu {
-  float: left;
-  padding-right: 50px;
+
+.about__ul {
+	display: flex;
 }
-.about-gdays {
-  float: left;
+
+.about__ul.about__ul--mid {
+	margin: 0 0 13px;
 }
-.menu-footer::after {
-  content:'';
-  clear: left; 
+
+.about__router-li {
+	cursor: pointer;
 }
-.cs-menu a, .about-gdays a {
-  display: block;
-  padding: 0 0 7px 0;
-  color: #B8B8B8;
+
+.about__ul.about__ul--title {
+	margin-bottom: 20px;
+	color: #767676;
+}
+
+.about__p {
+	margin-top: 30px;
+	font-size: 11px;
+}
+
+.about__aside {
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-start;
+	width: 50%;
+}
+
+.about__aside .about__ul {
+	display: flex;
+	flex-direction: column;
+}
+
+.about__ul.about__ul--left {
+	margin-right: 50px;
+}
+
+.about__li.about__li--title {
+	margin: 0 0 20px;
+	font-size: 14px;
+	font-weight: 600;
+	color: #767676;
+}
+
+.about__aside .about__router-li {
+	margin-bottom: 10px;
+	font-weight: 800;
+}
+
+.about__divider {
+	margin: 0 10px;
 }
 </style>
