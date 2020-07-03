6c8334ab19b1f80d2392697531d478b2d25222c0
eotkd4791 <eotkd4791@gmail.com>
2020-07-04 01:33:12 +0900

    Made Code More Tidy

--- client/src/mixins/utilMethods.js
+++ client/src/mixins/utilMethods.js
@@ -1,18 +1,17 @@
 export default {
   methods: {
     movePage(to) {
-      if (to === '#') return;
-      if (this.$route.path !== to) {
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-        const nextPage = {
-          path: to
-        };
-        this.$router.push(nextPage);
-      }
+      if (to === '#' || this.$route.path === to) return;
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+      const nextPage = { path: to };
+      this.$router.push(nextPage);
+    },
+    setScrollLock() {
+      window.scrollTo(0, 0);
     },
   }
 }
