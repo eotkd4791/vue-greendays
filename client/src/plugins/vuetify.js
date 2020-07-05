15c1a9067f8c1f09e38e47a2a95d51719ac78cfb
eotkd4791 <eotkd4791@gmail.com>
2020-07-05 23:09:41 +0900

    Adjust Prettier

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: "fa"
-  }
+	icons: {
+		iconfont: 'fa',
+	},
 });
