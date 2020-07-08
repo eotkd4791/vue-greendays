b1348dabc56edcbf0aa0ca20ce69026bf4a09c50
eotkd4791 <eotkd4791@gmail.com>
2020-07-08 18:01:39 +0900

    Create Dummy QuickDelivery

---
+++ client/src/db/quickDelivery.js
@@ -0,0 +1,11 @@
+import dbPreorder from './preorder.js';
+
+const quickDelivery = [];
+
+quickDelivery.push(dbPreorder[dbPreorder.length - 1]);
+
+quickDeleivery.promoName = 'Quick Delivery';
+
+dbPreorder.pop();
+
+export default quickDeleivery;
