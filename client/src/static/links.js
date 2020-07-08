1143d0897271e83f4ef27c45ab5b0da177a57ad4
eotkd4791 <eotkd4791@gmail.com>
2020-07-08 18:03:21 +0900

    Create links.js

---
+++ client/src/static/links.js
@@ -0,0 +1,46 @@
+export default {
+	getCustomerLinks() {
+		return [
+			{
+				routePath: '/customerservice',
+				name: '고객센터',
+			},
+			{
+				routePath: '/customerservice/logi_guide',
+				name: '배송가이드',
+			},
+			{
+				routePath: '/customerservice/notice',
+				name: '공지사항',
+			},
+			{
+				routePath: '/customerservice/faq',
+				name: '자주묻는질문',
+			},
+			{
+				routePath: '/customerservice/terms',
+				name: '이용약관',
+			},
+			{
+				routePath: '/customerservice/private_info_policy',
+				name: '개인정보취급방침',
+			},
+		];
+	},
+	getUserInfoLinks() {
+		return [
+			{
+				routePath: '/user/edit',
+				name: '내 정보 관리',
+			},
+			{
+				routePath: '/user/total_order_refund',
+				name: '주문 및 반품 내역',
+			},
+			{
+				routePath: '/user/point',
+				name: '포인트',
+			},
+		];
+	},
+};
