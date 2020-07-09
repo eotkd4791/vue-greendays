35b0157a28ad97cbc856b23c5fdfeb300594e375
eotkd4791 <eotkd4791@gmail.com>
2020-07-10 01:21:50 +0900

    Update Introduction View

--- client/src/views/Introduction.vue
+++ client/src/views/Introduction.vue
@@ -1,26 +1,55 @@
 <template>
-  <div>
-    그린데이즈 소개
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptatum ducimus similique.
-  </div>
+	<ul class="introduction">
+		<li class="introduction__li">
+			<img src="@/assets/img/green-present.png" alt="green-present" class="introduction__img" />
+		</li>
+		<li class="introduction__li">신입 웹 프론트엔드 개발자 포트폴리오</li>
+		<li class="introduction__li">
+			<a href="https://www.reddays.kr/" class="introduction__a">레드데이즈</a> 웹사이트를 클론코딩하였습니다.
+		</li>
+	</ul>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-div {
-  display:flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  width: 100%;
-  height: 650px;
-  overflow: hidden;
+.introduction {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 650px;
+	margin-top: 115px;
+}
+.introduction__li {
+	font-size: 20px;
+	font-weight: 600;
+	margin-bottom: 20px;
 }
 
+.introduction__img {
+	width: 70px;
+	height: 70px;
+	animation-name: img-moving;
+	animation-duration: 1s;
+	animation-iteration-count: infinite;
+	animation-timing-function: ease-in;
+	animation-direction: alternate;
+}
+
+.introduction__a {
+	text-decoration-line: underline;
+}
+
+@keyframes img-moving {
+	from {
+		transform: skew(-10deg, -10deg) translate(60px, -60px);
+	}
+	to {
+		transform: skew(10deg, 10deg) translate(-60px, -60px);
+	}
+}
 </style>
