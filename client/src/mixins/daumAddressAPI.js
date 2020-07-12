38e5680c0eed2a213323381426e37313b687e017
eotkd4791 <eotkd4791@gmail.com>
2020-07-12 19:29:10 +0900

    Align code lines

--- client/src/mixins/daumAddressAPI.js
+++ client/src/mixins/daumAddressAPI.js
@@ -1,13 +1,13 @@
 export default {
-    methods: {
-        openAddressSearch() {
-            new daum.Postcode({
-                oncomplete: (data => {
-                    this.userInfo.postCode = data.zonecode;
-                    this.userInfo.address = `${data.roadAddress} ${data.buildingName}`;
-                }),
-                animation: true,
-            }).open();
-        }
-    }
-}
+	methods: {
+		openAddressSearch() {
+			new daum.Postcode({
+				oncomplete: data => {
+					this.userInfo.postCode = data.zonecode;
+					this.userInfo.address = `${data.roadAddress} ${data.buildingName}`;
+				},
+				animation: true,
+			}).open();
+		},
+	},
+};
