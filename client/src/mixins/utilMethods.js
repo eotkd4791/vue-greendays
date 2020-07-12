ed9ae2859c5a8252098284ebef20f5f92f8fe77d
eotkd4791 <eotkd4791@gmail.com>
2020-07-12 19:29:44 +0900

    Exclude a function setScrollLock

--- client/src/mixins/utilMethods.js
+++ client/src/mixins/utilMethods.js
@@ -1,17 +1,14 @@
 export default {
-  methods: {
-    movePage(to) {
-      if (to === '#' || this.$route.path === to) return;
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
-      const nextPage = { path: to };
-      this.$router.push(nextPage);
-    },
-    setScrollLock() {
-      window.scrollTo(0, 0);
-    },
-  }
-}
+	methods: {
+		movePage(to) {
+			if (to === '#' || this.$route.path === to) return;
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: 'smooth',
+			});
+			const nextPage = { path: to };
+			this.$router.push(nextPage);
+		},
+	},
+};
