80543890d841d60dfdb23dff6c930cbdf8459803
eotkd4791 <eotkd4791@gmail.com>
2020-07-16 16:45:38 +0900

    Remove Redundancy & Fixed methods from faker.js

--- client/src/db/product.js
+++ client/src/db/product.js
@@ -1,31 +1,30 @@
-import faker from 'faker';
 import searchOrderBy from '@/static/orderby.js';
+import { Faker, getRandomNumber } from '@/utils/dummy.js';
 
 const gender = searchOrderBy.genders;
 const category = searchOrderBy.categories;
 const categoryIndex = ['Bags', 'Wallets', 'Clothes', 'Shoes', 'Accessories'];
-
-const getRandomNumber = limit => Math.floor(Math.random() * limit);
 
 const productList = [];
 
 function makeProductData() {
 	for (let i = 0; i < 400; i++) {
 		const categoryDetail = category[categoryIndex[getRandomNumber(5)]];
-
+		const priceBefore = Math.floor(Faker.commerce.price());
+		const discountRate = parseFloat(Math.random().toFixed(2));
 		productList.push({
 			id: getRandomNumber(20200707),
-			name: `${faker.commerce.productName() + i}`,
-			brand: faker.commerce.productAdjective(),
-			photoUrl: faker.image.avatar(),
+			name: `${Faker.commerce.productName() + i}`,
+			brand: Faker.commerce.productAdjective(),
+			photoUrl: Faker.image.avatar(),
 			category: categoryDetail[getRandomNumber(categoryDetail.length)],
-			gender: gender[getRandomNumber(4) + 1],
-			priceBefore: faker.commerce.price(),
-			discountRate: parseFloat(Math.random().toFixed(2)),
+			gender: gender[getRandomNumber(3) + 1],
+			priceBefore,
+			discountRate,
 			priceAfter: Math.floor(
-				this.priceBefore - this.priceBefore * this.discountRate,
+				priceBefore - priceBefore * discountRate,
 			).toLocaleString(),
-			material: faker.commerce.material(),
+			material: Faker.commerce.productMaterial(),
 			popularity: getRandomNumber(100),
 			productSendToday: getRandomNumber(100) % 2 ? true : false,
 			productAbleToBuy: getRandomNumber(100) % 2 ? false : true,
@@ -33,8 +32,6 @@ function makeProductData() {
 	}
 }
 
-const storedProduct = JSON.parse(localStorage.getItem('product'));
-
-!storedProduct ? makeProductData() : (productList = storedProduct);
+makeProductData();
 
 export default productList;
