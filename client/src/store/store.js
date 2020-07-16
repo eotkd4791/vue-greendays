bf9989400bf01bff1473135fe206d678278d468c
eotkd4791 <eotkd4791@gmail.com>
2020-07-16 16:43:44 +0900

    Modularization store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,16 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import auth from './modules/auth';
+import shopping from './modules/shopping';
+
+Vue.use(Vuex);
 
 import state from './state.js';
 import mutations from './mutations.js';
 import getters from './getters.js';
 import actions from './actions.js';
 
-Vue.use(Vuex);
-
 export default new Vuex.Store({
-  state,
-  mutations,
-  getters,
-  actions,
+	modules: {
+		auth: {
+			namespaced: true,
+			...auth,
+		},
+		shopping: {
+			namespaced: true,
+			...shopping,
+		},
+	},
+	state,
+	mutations,
+	getters,
+	actions,
 });
