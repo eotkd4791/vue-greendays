3ef178a64bd39180e7ab43d3ab561edabba0049b
eotkd4791 <eotkd4791@gmail.com>
2020-07-16 16:43:26 +0900

    Adjust Prettier & Fixed broken layout with z-index

--- client/src/views/CustomerService.vue
+++ client/src/views/CustomerService.vue
@@ -1,28 +1,27 @@
 <template>
-  <section class="customer-service__container">
-    <left-link />
-    <router-view />
-  </section>
+	<section class="customer-service__container">
+		<left-link />
+		<router-view />
+	</section>
 </template>
 
 <script>
 import LeftLink from '@/components/common/LeftLink.vue';
 
 export default {
-  components: {
-    LeftLink,
-  },
-}
+	components: {
+		LeftLink,
+	},
+};
 </script>
 
 <style scoped>
 .customer-service__container {
-  display:flex;
-  justify-content: space-between;
-  align-items: flex-start;
-  max-width: 1100px;
-  min-height: 532px;
-  margin: 150px auto 0;
-  z-index: 1001;
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	max-width: 1100px;
+	min-height: 532px;
+	margin: 150px auto 0;
 }
 </style>
