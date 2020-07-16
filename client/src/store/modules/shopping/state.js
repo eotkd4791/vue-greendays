dced747037878e87ff1a5a400000580b0c22e3af
eotkd4791 <eotkd4791@gmail.com>
2020-07-16 16:41:30 +0900

    Create shopping/state.js

---
+++ client/src/store/modules/shopping/state.js
@@ -0,0 +1,9 @@
+const state = () => ({
+	products: [],
+	hotKeywords: ['발렌시아가', '프라다', '버버리', '지갑', '생로랑', '클러치'],
+	recommendKeywords: [],
+	totalKeywords: [],
+	recentKeywords: new Map(),
+});
+
+export default state;
