679fb464096fd3bf8a96fe708edc8ce095d33bac
eotkd4791 <eotkd4791@gmail.com>
2020-07-17 19:34:55 +0900

    Adjust Prettier

--- client/src/views/UserInfoEdit.vue
+++ client/src/views/UserInfoEdit.vue
@@ -1,27 +1,29 @@
 <template>
-  <section class="user-info-edit">
-    <header class="user-info-edit__header">내 정보 관리</header>
-    <ul class="user-info-edit__ul">
-      <info-list :eachOfListItems="{ title: '내 정보 수정' }">
-        <template #edit-user-info>
-          <edit-user-info-list />
-        </template>
-      </info-list>
-      <info-list :eachOfListItems="{ title: '배송지 관리' }">
-        <template #shipping-address-setting>
-          <shipping-address-setting />
-        </template>
-      </info-list>
-      <info-list :eachOfListItems="{ title: '추천코드' }">
-        <template #user-promotion-code>
-          <user-promotion-code />
-        </template>
-      </info-list>
-      <li class="user-info-edit__li">
-        <button class="user-info-edit__btn" @click="deleteMyAccount">회원탈퇴</button>
-      </li>
-    </ul>
-  </section>
+	<section class="user-info-edit">
+		<header class="user-info-edit__header">내 정보 관리</header>
+		<ul class="user-info-edit__ul">
+			<info-list :eachOfListItems="{ title: '내 정보 수정' }">
+				<template #edit-user-info>
+					<edit-user-info-list />
+				</template>
+			</info-list>
+			<info-list :eachOfListItems="{ title: '배송지 관리' }">
+				<template #shipping-address-setting>
+					<shipping-address-setting />
+				</template>
+			</info-list>
+			<info-list :eachOfListItems="{ title: '추천코드' }">
+				<template #user-promotion-code>
+					<user-promotion-code />
+				</template>
+			</info-list>
+			<li class="user-info-edit__li">
+				<button class="user-info-edit__btn" @click="deleteMyAccount">
+					회원탈퇴
+				</button>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <script>
@@ -31,53 +33,59 @@ import ShippingAddressSetting from '@/components/ShippingAddressSetting.vue';
 import UserPromotionCode from '@/components/UserPromotionCode.vue';
 
 export default {
-  components: {
-    InfoList,
-    EditUserInfoList,
-    ShippingAddressSetting,
-    UserPromotionCode
-  },
-  methods: {
-    deleteMyAccount() {
-      const answer = confirm('정말 탈퇴하시겠습니까 ?');
-      if(answer) {
-        // 탈퇴 로직 구현하기
-      }
-    }
-  }
-}
+	components: {
+		InfoList,
+		EditUserInfoList,
+		ShippingAddressSetting,
+		UserPromotionCode,
+	},
+	methods: {
+		deleteMyAccount() {
+			const confirmQuitMemberShip = confirm('정말 탈퇴하시겠습니까 ?');
+			if (confirmQuitMemberShip) {
+				this.$store
+					.dispatch('auth/QUIT_MEMBERSHIP')
+					.then(() => {
+						this.$router.replace('/');
+					})
+					.catch(error => {
+						console.error(error);
+					});
+			}
+		},
+	},
+};
 </script>
 
 <style scoped>
 .user-info-edit {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  width: 840px;
-  min-height: 433px;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
+	width: 840px;
+	min-height: 433px;
 }
 
 .user-info-edit__header {
-  font-size: 20px;
-  margin: 0 0 36px;
+	font-size: 20px;
+	margin: 0 0 36px;
 }
 
 .user-info-edit__ul {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
 }
 
 .user-info-edit__btn {
-  width: 100%;
-  margin: 30px 0 0;
-  color: #b6b6b6;
-  font-size: 14px;
-  text-decoration: underline;
-  cursor: pointer;
+	width: 100%;
+	margin: 30px 0 0;
+	color: #b6b6b6;
+	font-size: 14px;
+	text-decoration: underline;
+	cursor: pointer;
 }
-
 </style>
