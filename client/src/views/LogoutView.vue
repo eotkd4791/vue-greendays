a9a3618ea47376f687a7a1af64570f443e0cc660
eotkd4791 <eotkd4791@gmail.com>
2020-07-21 00:24:45 +0900

    Create LogoutView.vue

---
+++ client/src/views/LogoutView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="log-out" />
+</template>
+
+<style scoped>
+.log-out {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	z-index: 9999;
+	background-color: #fff;
+}
+</style>
