bead2687b18e7945623d3135bd1af5fbb2cdb9f3
eotkd4791 <eotkd4791@gmail.com>
2020-07-21 00:27:29 +0900

    Update Code For Better Readability & Adjust Semantic Markup & BEM ClassNaming

--- client/src/components/common/BrandModal.vue
+++ client/src/components/common/BrandModal.vue
@@ -1,178 +1,184 @@
 <template>
-  <transition name="modal">
-    <div class="modal-mask">
-      <div class="modal-wrapper" @click.stop="onClickOutside">
-        <div class="modal-container">
-          <div class="modal-header">
-            <header class="brand-modal-title">브랜드 검색</header>
-            <button class="modal-default-button" @click="$emit('closeBrands')">
-              <i class="fas fa-times" />
-            </button>
-          </div>
+	<transition name="modal">
+		<div class="brand-modal__mask">
+			<div class="brand-modal__background" @click="onClickOutside">
+				<div class="brand-modal">
+					<section class="brand-modal__section">
+						<header class="brand-modal__header">브랜드 검색</header>
+						<button class="brand-modal__btn" @click="$emit('closeBrands')">
+							<i class="fas fa-times" />
+						</button>
+					</section>
 
-          <div class="modal-body">
-            <div class="custom-pick-menu" ref="brands">
-              <div class="brands-active" @click="changeTab">인기브랜드</div>|
-              <div @click="changeTab">전체브랜드</div>
-            </div>
-            <pick-brand
-              v-if="showModal"
-              :propsBrand="pickedBrand"
-              @closePickedBrands="closePickBrand"
-            />
-            <popular-brands v-if="pickBrands" @openAlarmModal="openPickBrand" />
-            <alphabet-brands v-else @openAlarmModal="openPickBrand" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </transition>
+					<aside class="brand-modal__aside">
+						<ul class="brand-modal__ul" ref="brands">
+							<li class="brand-modal__menu--active" @click="changeTab">인기브랜드</li>|
+							<li @click="changeTab">전체브랜드</li>
+						</ul>
+						<pick-brand v-if="showModal" :propsBrand="pickedBrand" />
+						<popular-brands v-if="isShownPopularBrand" @openAlarmModal="openPickBrand" />
+						<alphabet-brands v-else @openAlarmModal="openPickBrand" />
+					</aside>
+				</div>
+			</div>
+		</div>
+	</transition>
 </template>
 
 <script>
 import PickBrand from './PickBrandModal.vue';
 import AlphabetBrands from '../AlphabetBrands.vue';
 import PopularBrands from '../PopularBrands.vue';
+import Bus from '@/utils/bus.js';
 
 export default {
-  components: {
-    PickBrand,
-    AlphabetBrands,
-    PopularBrands,
-  },
-  data() {
-    return {
-      user: {},
-      pickBrands: true,
-      showModal: false,
-      pickedBrand: ''
-    };
-  },
-  methods: {
-    onClickOutside(e) {
-      if(e.target.className ==='modal-wrapper') {
-        this.$emit('closeBrands');
-      }
-    },
-    changeTab() {
-      this.pickBrands = !this.pickBrands;
-      this.$refs.brands.children[0].classList.toggle('brands-active');
-      this.$refs.brands.children[1].classList.toggle('brands-active');
-    },
-    openPickBrand(payload) {
-      this.showModal = true;
-      this.pickedBrand = payload;
-    },
-    closePickBrand() {
-      this.showModal = false;
-    },
-    setScrollLock() {
-      window.scrollTo(0, 0);
-    },
-  },
-  created() {
-    this.pickBrands = true;
-    this.user = this.$store.state.userInfo;
-    // 생성되자마자 로그인 여부로 화면에 보여줄 모달 선택하는 로직 구현하기
+	components: {
+		PickBrand,
+		AlphabetBrands,
+		PopularBrands,
+	},
 
-    //알파벳 선택하는 로직 -> 디비에서 불러온 브랜드 이름이 하나도 없으면 passive라는 클래스를 추가한다.(cursor: not-allowed)
-    //
-  },
-  mounted() {
-    window.addEventListener('scroll', this.setScrollLock);
-  },
-  beforeDestroy() {
-    window.removeEventListener('scroll', this.setScrollLock);
-  }
-}
+	data() {
+		return {
+			isShownPopularBrand: true,
+			showModal: false,
+			pickedBrand: '',
+		};
+	},
+
+	methods: {
+		onClickOutside(e) {
+			if (e.target.className === 'brand-modal__background') {
+				this.$emit('closeBrands');
+			}
+		},
+
+		changeTab() {
+			this.isShownPopularBrand = !this.isShownPopularBrand;
+			const { children } = this.$refs.brands;
+			children[0].classList.toggle('brand-modal__menu--active');
+			children[1].classList.toggle('brand-modal__menu--active');
+		},
+
+		openPickBrand(payload) {
+			this.showModal = true;
+			this.pickedBrand = payload;
+		},
+
+		closePickBrand() {
+			this.showModal = false;
+		},
+
+		setScrollLock(e) {
+			scrollTo(0, 0);
+		},
+	},
+
+	mounted() {
+		Bus.$on('off:picked-brands', this.closePickBrand);
+		window.addEventListener('scroll', this.setScrollLock);
+	},
+
+	beforeDestroy() {
+		Bus.$off('off:picked-brands', this.closePickBrand);
+		window.removeEventListener('scroll', this.setScrollLock);
+	},
+};
 </script>
 
 <style scoped>
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
+.brand-modal__mask {
+	position: fixed;
+	z-index: 9998;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: table;
+	transition: opacity 0.3s ease;
 }
 
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
+.brand-modal__background {
+	display: table-cell;
+	vertical-align: middle;
 }
 
-.modal-container {
-  width: 420px;
-  height: 612px;
-  margin: 0px auto;
-  background-color: #F7F7F7;
-  border-radius: 10px;
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
+.brand-modal {
+	width: 420px;
+	height: 612px;
+	margin: 0px auto;
+	background-color: #f7f7f7;
+	border-radius: 10px;
+	transition: all 0.3s ease;
 }
 
-.modal-header {
-  width: 420px;
-  height: 80px;
-  margin-bottom: 10px;
-  padding: 0 30px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #fff;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+.brand-modal__section {
+	width: 420px;
+	height: 80px;
+	margin-bottom: 10px;
+	padding: 0 30px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #fff;
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
 }
 
-.modal-header>header{
-  font-weight: 800;
-  font-size: 18px;
+.brand-modal__header {
+	font-weight: 800;
+	font-size: 18px;
 }
-.modal-body {
-  height: 520px;
-  padding: 0 30px;
-  background-color: #fff;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-}
-
-.custom-pick-menu {
-  width: 360px;
-  height: 60px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  font-size: 12px;
-  font-weight: 600;
-  cursor: pointer;
-  color: #bbbbbb;
-  border-bottom: 1px solid #EDEDED;
-}
-.custom-pick-menu div {
-  width: 179.5px;
-  height: 60px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-.brands-active {
-  color: #42b883;
+.brand-modal__aside {
+	height: 520px;
+	padding: 0 30px;
+	background-color: #fff;
+	border-bottom-left-radius: 10px;
+	border-bottom-right-radius: 10px;
 }
 
-.modal-default-button {
-  float: right;
-  border-style: none;
-  outline: none;
+.brand-modal__ul {
+	width: 360px;
+	height: 60px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	font-size: 12px;
+	font-weight: 600;
+	cursor: pointer;
+	color: #bbbbbb;
+	border-bottom: 1px solid #ededed;
 }
-.modal-enter { opacity: 0; }
-.modal-leave-active { opacity: 0; }
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
+
+.brand-modal__ul li {
+	width: 179.5px;
+	height: 60px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+
+.brand-modal__menu--active {
+	color: #42b883;
+}
+
+.brand-modal__btn {
+	border-style: none;
+	outline: none;
+}
+
+.modal-enter {
+	opacity: 0;
+}
+
+.modal-leave-active {
+	opacity: 0;
+}
+
+.modal-enter .brand-modal,
+.modal-leave-active .brand-modal {
+	-webkit-transform: scale(1.1);
+	transform: scale(1.1);
 }
 </style>
