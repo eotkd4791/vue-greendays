69df2808e94a714e4450bcd99b2d7f9866727328
eotkd4791 <eotkd4791@gmail.com>
2020-07-22 15:08:00 +0900

    Add font

--- client/src/db/preorder.js
+++ client/src/db/preorder.js
@@ -1,10 +1,18 @@
 import dbProduct from '@/db/product.js';
 import { Faker, getRandomNumber } from '@/utils/dummy.js';
 
-let preorderList = [];
+const preorderList = [];
+const products = dbProduct.sort((a, b) => (a.productAbleToBuy ? -1 : 1));
 
-function makePreorderData() {
-	dbProduct.sort(v => (v.productAbleToBuy ? 1 : -1));
+const preorderImgPath = [
+	'https://menu.mt.co.kr/moneyweek/thumb/2019/09/27/06/2019092710298098227_1.jpg',
+	'https://image.fmkorea.com/files/attach/new/20200107/4477817/5151653/2578465312/abe9caf1dcacaa555a695b5fa39d16a0.jpg',
+	'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F190212464FDEB31123',
+	'https://t1.daumcdn.net/liveboard/maxim/985c9936d6854c27ab701523395a4fd4.png',
+	'https://press.farm/wp-content/uploads/2018/03/fashion-luxury-brands-marketing.jpg',
+];
+
+function createPreorders() {
 	for (let i = 0; i < 5; i++) {
 		preorderList.push({
 			title: Faker.lorem.slug(),
@@ -14,15 +22,12 @@ function makePreorderData() {
 			activated: true,
 			promoName:
 				getRandomNumber(2) % 2 ? '2020 S/S COLLECTION' : '2020 F/W COLLECTION',
-			imgUrl: Faker.image.fashion(),
-			products: dbProduct.slice(i * 80, (i + 1) * 80),
+			imgUrl: preorderImgPath[i],
+			products: products.slice(i * 80, (i + 1) * 80),
 		});
 	}
-	localStorage.setItem('preorders', JSON.stringify(preorderList));
 }
 
-const storedPreorder = JSON.parse(localStorage.getItem('preorders'));
-
-!!storedPreorder ? makePreorderData() : (preorderList = storedPreorder);
+createPreorders();
 
 export default preorderList;
