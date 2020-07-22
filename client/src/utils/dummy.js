c6933483671cec12a82e4e9e894ff99fd8d6db7f
eotkd4791 <eotkd4791@gmail.com>
2020-07-22 15:29:07 +0900

    Update Function CreatePromotionCode To Fix 6 Hex Digits

--- client/src/utils/dummy.js
+++ client/src/utils/dummy.js
@@ -5,9 +5,14 @@ export const Faker = faker;
 export const getRandomNumber = limit => Math.floor(Math.random() * limit);
 
 export const createPromotionCode = () => {
-	const limit = 1000000;
-	const sixRandomDigit = getRandomNumber(limit) + limit;
-	return (sixRandomDigit < limit ? sixRandomDigit : sixRandomDigit - limit)
+	const maximumHex = 16777216; //0xFFFFFF + 1
+	const minimumHex = 1048576; //0x100000
+	const sixRandomDigit = getRandomNumber(maximumHex) + minimumHex;
+	console.log(sixRandomDigit);
+	return (sixRandomDigit <= maximumHex
+		? sixRandomDigit
+		: sixRandomDigit - minimumHex
+	)
 		.toString(16)
 		.toUpperCase();
 };
