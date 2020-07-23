3f291a5bb130887565e061a98e53b41e43abbed0
eotkd4791 <eotkd4791@gmail.com>
2020-07-23 19:21:32 +0900

    Add FETCH_PREORDERS, FETCH_ORDERED_PRODUCTS & Fixed SET_EXPECTED_KEYWORDS, FETCH_RECOMMEND_KEYWORDS

--- client/src/store/modules/shopping/actions.js
+++ client/src/store/modules/shopping/actions.js
@@ -1,56 +1,150 @@
 import { getItem, setItem } from '@/utils/storage.js';
 import dbProduct from '@/db/product.js';
+import dbPreorder from '@/db/preorder.js';
 
 const actions = {
-	FETCH_ALL_PRODUCT({ commit }) {
+	// 모든 상품 가져오기
+	FETCH_ALL_PRODUCTS({ commit }) {
 		const storedProduct = getItem('product');
-		if (!storedProduct) {
-			setItem('product', storedProduct ? storedProduct : dbProduct);
-			commit('setProducts', storedProduct ? storedProduct : dbProduct);
-		}
+		setItem('product', !!storedProduct ? storedProduct : dbProduct);
+		commit('setProducts', !!storedProduct ? storedProduct : dbProduct);
 	},
 
-	SET_EXPECTED_KEYWORDS({ state, commit }) {
+	// 전체 상품의 정보를 가져와 저장한다.
+	SET_EXPECTED_KEYWORDS({ commit }) {
 		const storedProduct = getItem('product');
 		const storedKeywords = getItem('totalKeywords');
 
 		if (!storedKeywords) {
-			const totalKeywords = [];
+			let totalKeywords = [];
 			storedProduct.map(v => {
 				const { brand, category, material, name } = v;
 				totalKeywords = totalKeywords.concat(
-					brand,
-					material,
-					category.split(' '),
-					name.split(' '),
+					brand.toUpperCase(),
+					material.toUpperCase(),
+					category.toUpperCase().split(' '),
+					name.toUpperCase().split(' '),
 				);
 			});
 			totalKeywords = [...new Set(totalKeywords)].sort();
 			setItem('totalKeywords', totalKeywords);
+			setItem('recentKeywords', null);
 			commit('setTotalKeywords', totalKeywords);
 		} else {
 			commit('setTotalKeywords', storedKeywords);
 		}
 	},
 
-	FETCH_RECOMMEND_KEYWORDS({ state, commit }, payload) {
+	//검색한 키워드와 연관 키워드 뽑기
+	FETCH_RECOMMEND_KEYWORDS({ commit }, payload) {
 		const storedKeywords = getItem('totalKeywords');
-		const storedRecentKeywords = new Map(getItem('recentKeywords'));
+		const storedRecentKeywords = new Map(
+			Object.entries(getItem('recentKeywords')),
+		);
 
 		if (!storedRecentKeywords.has(payload)) {
 			const croppedKeywords = storedKeywords
 				.map(v => v.includes(payload) && v)
 				.filter(v => v);
 
-			commit('setRecentKeyword', [payload, croppedKeywords]);
+			commit('setRecentKeywords', { key: payload, value: croppedKeywords });
+			commit('setRecommendKeywords', croppedKeywords);
 			setItem(
 				'recentKeywords',
 				storedRecentKeywords.set(payload, croppedKeywords),
 			);
 		} else {
+			commit('setRecentKeywords', storedRecentKeywords.get(payload));
+			commit('setRecommendKeywords', storedRecentKeywords.get(payload));
 		}
-		//key: 검색어(payload), value: 찾은 결과(croppedKeywords)
-		// setItem('recentKeywords', storedRecentKeywords ? croppedKeyword)
+	},
+
+	FETCH_PREORDERS({ commit }) {
+		const storedPreorders = getItem('preorder');
+		if (!!storedPreorders) {
+			setItem('preorder', storedPreorders);
+			commit('setPreorders', storedPreorders);
+		} else {
+			setItem('preorder', dbPreorder);
+			commit('setPreorders', dbPreorder);
+		}
+	},
+
+	FETCH_ORDERED_PRODUCTS({ state, commit }, query) {
+		const orderConditions = Object.entries(query).filter(v => v[1].length > 0);
+		let productsAll = [...state.products];
+
+		orderConditions.forEach(entry => {
+			switch (entry[0]) {
+				case 'keyword':
+					productsAll = productsAll.filter(value => {
+						const { brand, category, material, name } = value;
+						if (
+							new RegExp(brand, 'ig').test(entry[1]) ||
+							new RegExp(category, 'ig').test(entry[1]) ||
+							new RegExp(material, 'ig').test(entry[1]) ||
+							new RegExp(name, 'ig').test(entry[1])
+						) {
+							return value;
+						}
+					});
+					break;
+
+				case 'brand':
+					productsAll = productsAll.filter(value => {
+						const { brand } = value;
+						if (new RegExp(brand, 'ig').test(entry[1])) {
+							return value;
+						}
+					});
+					break;
+
+				case 'gender':
+					productsAll = productsAll.filter(value => {
+						const { gender } = value;
+						if (new RegExp(gender, 'ig').test(entry[1])) {
+							return value;
+						}
+					});
+					break;
+
+				case 'category':
+					productsAll = productsAll.filter(value => {
+						const { category } = value;
+						if (new RegExp(category, 'ig').test(entry[1])) {
+							return value;
+						}
+					});
+					break;
+
+				case 'category_detail':
+					productsAll = productsAll.filter(value => {
+						const { categoryDetail } = value;
+						if (new RegExp(categoryDetail, 'ig').test(entry[1])) {
+							return value;
+						}
+					});
+					break;
+
+				case 'deal_id':
+					productsAll = productsAll.filter(value =>
+						entry[1] === 'reserve-purchase'
+							? value.productAbleToBuy
+							: value.productSendToday,
+					);
+					break;
+
+				case 'product_id':
+					productsAll = productsAll.filter(value => {
+						const { id } = value;
+						if (new RegExp(id, 'ig').test(entry[1])) {
+							return value;
+						}
+					});
+					break;
+			}
+		});
+		commit('setOrderedProduct', productsAll);
 	},
 };
 
