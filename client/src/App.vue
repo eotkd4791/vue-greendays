94f2257c37595e1534f1b12b003091302cc9992d
eotkd4791 <eotkd4791@gmail.com>
2020-07-23 19:24:10 +0900

    Change Path Of SyncLoader.vue & Add Transition & CSS For Space From Toolbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <template>
 	<main class="main" onselectstart="return false">
 		<toolbar-view />
-		<div v-if="onSpace" class="space" />
-		<sync-loader :loading="isShownSpinner"></sync-loader>
-		<router-view />
+		<div class="space" :class="{ 'space--active': onSpace }" />
+		<sync-loader :loading="isShownSpinner" :size="'30px'" :margin="'10px'" />
+		<transition name="fade">
+			<router-view />
+		</transition>
 		<footer-view />
 	</main>
 </template>
@@ -13,13 +15,13 @@ import ToolbarView from '@/views/ToolbarView.vue';
 import FooterView from '@/views/FooterView.vue';
 import Bus from '@/utils/bus.js';
 import { mapActions } from 'vuex';
-import SyncLoader from 'vue-spinner/src/SyncLoader.vue';
+import SyncLoader from '@/components/common/SyncLoader.vue';
 
 export default {
 	components: {
 		ToolbarView,
-		FooterView,
 		SyncLoader,
+		FooterView,
 	},
 	data() {
 		return {
@@ -30,21 +32,28 @@ export default {
 	methods: {
 		...mapActions({
 			IS_LOGGED_IN: 'auth/IS_LOGGED_IN',
-			FETCH_ALL_PRODUCT: 'shopping/FETCH_ALL_PRODUCT',
+			FETCH_ALL_PRODUCTS: 'shopping/FETCH_ALL_PRODUCTS',
+			FETCH_PREORDERS: 'shopping/FETCH_PREORDERS',
 		}),
 
 		toggleSpace() {
 			this.onSpace = !this.onSpace;
 		},
+
 		onSpinner() {
 			this.isShownSpinner = true;
 		},
+
 		offSpinner() {
 			this.isShownSpinner = false;
 		},
 	},
+
 	created() {
-		this.FETCH_ALL_PRODUCT();
+		this.onSpinner();
+		this.FETCH_ALL_PRODUCTS()
+			.then(() => this.FETCH_PREORDERS())
+			.then(() => this.offSpinner());
 	},
 	mounted() {
 		Bus.$on('showUserInfo', this.toggleSpace);
@@ -56,18 +65,35 @@ export default {
 
 <style>
 @import './style/reset.css';
+
 @font-face {
 	font-family: 'Montserrat';
 	src: url('./assets/fonts/Montserrat-Light.ttf');
 }
+
 .main {
 	display: flex;
 	flex-direction: column;
 	width: 100%;
 	margin: 0 auto;
 }
+
 .space {
 	width: 100%;
+	height: 150px;
+}
+
+.space.space--active {
 	height: 280px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+	opacity: 0;
 }
 </style>
