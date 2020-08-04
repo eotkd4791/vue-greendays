4395ba820b0a7876d67a97210437acc0ca4b2812
eotkd4791 <eotkd4791@gmail.com>
2020-08-04 19:56:41 +0900

    Update Products Default Value & Add Properties

--- client/src/store/modules/shopping/state.js
+++ client/src/store/modules/shopping/state.js
@@ -1,9 +1,14 @@
 const state = () => ({
-	products: [],
+	products: null,
+	orderedProducts: [],
 	hotKeywords: ['발렌시아가', '프라다', '버버리', '지갑', '생로랑', '클러치'],
 	recommendKeywords: [],
 	totalKeywords: [],
 	recentKeywords: new Map(),
+	guestKeywords: [],
+	brandList: [],
+	preorders: [],
+	productDetail: {},
 });
 
 export default state;
