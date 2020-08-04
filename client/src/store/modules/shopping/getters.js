e81370a9fc7e5a2e8c44eb5a25efbac8ab356fdc
eotkd4791 <eotkd4791@gmail.com>
2020-08-04 19:54:21 +0900

    Add getTotalKeywords & getGuestKeywords

--- client/src/store/modules/shopping/getters.js
+++ client/src/store/modules/shopping/getters.js
@@ -8,6 +8,12 @@ const getters = {
 	getRecentKeywords(state) {
 		return [...state.recentKeywords.entries()];
 	},
+	getTotalKeywords(state) {
+		return state.totalKeywords;
+	},
+	getGuestKeywords(state) {
+		return state.guestKeywords;
+	},
 };
 
 export default getters;
