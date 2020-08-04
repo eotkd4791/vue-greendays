d239968dc2b7be0ad99406a60172847307a32155
eotkd4791 <eotkd4791@gmail.com>
2020-08-04 20:02:18 +0900

    Add Comments

--- client/src/store/modules/auth/actions.js
+++ client/src/store/modules/auth/actions.js
@@ -1,9 +1,10 @@
 import { getItem, setItem } from '@/utils/storage.js';
 import dbUserList from '@/db/userList.js';
+import { isEmpty } from 'lodash';
 
 const actions = {
+	//유저목록 가져오기
 	FETCH_USER_LIST({ state, commit }) {
-		//유저목록 가져오기
 		try {
 			const storedUserList = getItem('userList');
 			if (storedUserList) {
@@ -19,6 +20,7 @@ const actions = {
 		}
 	},
 
+	//이메일 중복확인
 	CHECK_EMAIL({ state, commit }, payload) {
 		try {
 			const existSameEmail = state.userList.find(
@@ -37,7 +39,8 @@ const actions = {
 		}
 	},
 
-	COMPARE_PROMOTION_CODE({ state, commit }, payload) {
+	// 추천 코드 비교
+	COMPARE_PROMOTION_CODE({ state }, payload) {
 		try {
 			const additionalPoint = 2000;
 			const payloadToCompare = payload.toUpperCase();
@@ -57,6 +60,7 @@ const actions = {
 		}
 	},
 
+	//회원가입
 	SIGN_UP({ commit }, payload) {
 		try {
 			const storedUserList = getItem('userList');
@@ -71,6 +75,7 @@ const actions = {
 		}
 	},
 
+	//로그인
 	LOG_IN({ state, commit }, payload) {
 		try {
 			const existUser = state.userList.find(
@@ -88,6 +93,7 @@ const actions = {
 		}
 	},
 
+	// 로그아웃
 	LOG_OUT({ commit }) {
 		try {
 			setItem('userInfo', null);
@@ -97,6 +103,7 @@ const actions = {
 		}
 	},
 
+	//로그인 여부 확인
 	IS_LOGGED_IN({ commit }) {
 		try {
 			const storedUserInfo = getItem('userInfo');
@@ -111,17 +118,19 @@ const actions = {
 		}
 	},
 
+	//회원 탈퇴
 	QUIT_MEMBERSHIP({ commit }) {
 		const storedUserInfo = getItem('userInfo');
 		const storedUserList = getItem('userList');
 		const indexToDelete = storedUserList.indexOf(storedUserInfo);
 		storedUserList.splice(indexToDelete, 1);
-		setItem('userInfo', {});
+		setItem('userInfo', null);
 		setItem('userList', storedUserInfo);
-		commit('setUser', {});
+		commit('setUser', null);
 		commit('setUserList', storedUserList);
 	},
 
+	// 배송지 추가
 	ADD_NEW_SHIPPING_ADDRESS({ commit }, payload) {
 		const storedUserInfo = getItem('userInfo');
 		storedUserInfo.shippingAddressList.push(payload);
@@ -129,6 +138,7 @@ const actions = {
 		commit('addShippingAddress', payload);
 	},
 
+	// 배송지 삭제
 	DELETE_SHIPPING_ADDRESS({ commit }, payload) {
 		const storedUserInfo = getItem('userInfo');
 		storedUserInfo.shippingAddressList.splice(payload, 1);
@@ -136,26 +146,99 @@ const actions = {
 		commit('deleteShippingAddress', payload);
 	},
 
-	FETCH_SEARCH_KEYWORD({ commit }) {
-		const storedUserInfo = getItem('userInfo');
-		commit('setSearchedKeyword', storedUserInfo.searchedKeyword);
-	},
-
-	ADD_SEARCH_KEYWORD({ commit }, payload) {
-		const storedUserInfo = getItem('userInfo');
-		const isUnique = storedUserInfo.searchedKeyword.find(v => v === payload);
-		if (isUnique) {
-			storedUserInfo.searchedKeyword.unshift(payload);
-			setItem('userInfo', storedUserInfo);
-			commit('addSearchKeyword', payload);
+	// 최근 검색어 이력 가져오기
+	FETCH_SEARCHED_KEYWORD({ state, commit }) {
+		const { userInfo } = state;
+		if (userInfo) {
+			commit('setSearchedKeyword', userInfo.searchedKeyword);
+		} else {
+			const storedGuestKeyword = getItem('guestKeywords');
+			if (storedGuestKeyword) {
+				commit('shopping/setGuestKeywords', storedGuestKeyword, { root: true });
+			} else {
+				setItem('guestKeywords', []);
+			}
 		}
 	},
 
-	DELETE_ALL_SEARCH_KEYWORD({ commit }) {
+	ADD_SEARCH_KEYWORD({ state, commit, rootState }, payload) {
+		try {
+			const { userInfo } = state;
+			const { shopping } = rootState;
+			const isLoggedIn = !isEmpty(userInfo);
+			const isUnique = isLoggedIn
+				? userInfo.searchedKeywords.find(v => v === payload)
+				: shopping.guestKeywords.find(v => v === payload);
+
+			if (!isUnique) {
+				if (isLoggedIn) {
+					const storedUserKeywords = getItem('userInfo');
+					storedUserKeywords.searchedKeywords.unshift(payload);
+					setItem('userInfo', storedUserKeywords);
+					commit('addUserKeyword', payload);
+				} else {
+					const storedGuestKeywords = getItem('guestKeywords');
+					storedGuestKeywords.unshift(payload);
+					setItem('guestKeywords', storedGuestKeywords);
+					commit('shopping/addGuestKeyword', payload, { root: true });
+				}
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	},
+
+	DELETE_ALL_SEARCH_KEYWORDS({ commit }) {
 		const storedUserInfo = getItem('userInfo');
-		storedUserInfo.searchedKeyword = [];
+		if (!isEmpty(storedUserInfo)) {
+			storedUserInfo.searchedKeywords = [];
+			setItem('userInfo', storedUserInfo);
+			commit('deleteAllSearchKeywords');
+		} else {
+			setItem('guestKeywords', null);
+			commit('shopping/deleteAllGuestKeywords', null, { root: true });
+		}
+	},
+
+	UPDATE_PICKED_BRANDS({ commit }, { key, value }) {
+		const storedUserInfo = getItem('userInfo');
+		const existPickedCategory =
+			Object.values(value).filter(v => v).length > 0 ? true : false;
+
+		existPickedCategory
+			? (storedUserInfo.pickedBrands[key] = value)
+			: delete storedUserInfo.pickedBrands[key];
+
 		setItem('userInfo', storedUserInfo);
-		commit('deleteAllSearchKeyword');
+		commit('setUser', storedUserInfo);
+	},
+
+	DELETE_PICKED_BRANDS({ commit }, payload) {
+		const storedUserInfo = getItem('userInfo');
+
+		payload.forEach(v => {
+			delete storedUserInfo.pickedBrands[v];
+		});
+
+		setItem('userInfo', storedUserInfo);
+		commit('setUser', storedUserInfo);
+	},
+
+	ADD_WISHLIST({ commit }, payload) {
+		const storedUserInfo = getItem('userInfo');
+		storedUserInfo.wishList.push(payload);
+		setItem('userInfo', storedUserInfo);
+		commit('setUser', storedUserInfo);
+	},
+
+	DELETE_WISHLIST({ commit }, payload) {
+		const storedUserInfo = getItem('userInfo');
+		payload.forEach(v => {
+			const indexToDelete = storedUserInfo.wishList.findIndex(o => o.id === v);
+			storedUserInfo.wishList.splice(indexToDelete, 1);
+		});
+		setItem('userInfo', storedUserInfo);
+		commit('setUser', storedUserInfo);
 	},
 };
 
