aee67ad286bf4f8ea0e0235d3b2a90c933dd9f38
eotkd4791 <eotkd4791@gmail.com>
2020-08-04 20:01:50 +0900

    Adjust Prettier & Change ClassName

--- client/src/components/SortIndexChips.vue
+++ client/src/components/SortIndexChips.vue
@@ -1,35 +1,38 @@
 <template>
-  <div class="chip-container">
-    {{ sortindex }}
-    <i class="fas fa-times-circle" @click="removeOne(sortindex)" />
-  </div>
+	<div class="chip">
+		{{ sortindex }}
+		<i class="fas fa-times-circle" @click="removeOne(sortindex)" />
+	</div>
 </template>
 
 <script>
 export default {
-  props: ['sortindex'],
-  methods: {
-    removeOne(payload) {
-      this.$emit('remove-chips', payload);
-    }
-  }
-}
+	props: ['sortindex'],
+	methods: {
+		removeOne(payload) {
+			this.$emit('remove-chips', payload);
+		},
+	},
+};
 </script>
 
 <style scoped>
-.chip-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  /* width */
-  height: 30px;
-  border-radius: 15px;
-  margin: 0 10px 8px 0;
-  padding: 0 7px 0 15px;
-  background-color: #42b883;
-  color: #fff;
-  font-size: 16px;
-  font-weight: 600;
+.chip {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	height: 30px;
+	border-radius: 15px;
+	margin: 0 10px 8px 0;
+	padding: 0 7px 0 15px;
+	background-color: #42b883;
+	color: #fff;
+	font-size: 16px;
+	font-weight: 600;
 }
-.fas { margin-left: 10px; color: #fff; cursor: pointer; }
+.fas {
+	margin-left: 10px;
+	color: #fff;
+	cursor: pointer;
+}
 </style>
