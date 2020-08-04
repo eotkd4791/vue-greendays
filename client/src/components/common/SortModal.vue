0d4d1156014de8b60daa00290e18f45c2f0dadaa
eotkd4791 <eotkd4791@gmail.com>
2020-08-04 19:47:54 +0900

    Semantic MarkUp & Adjust Prettier

--- client/src/components/common/SortModal.vue
+++ client/src/components/common/SortModal.vue
@@ -1,86 +1,99 @@
 <template>
-  <div class="modal-container" ref="modal">
-    <div
-      class="modal-list"
-      v-for="value in whichModal"
-      :key="value"
-      @click="pickedOrderBy({value, detail: null})"
-      @mouseover="activatingTab"
-    >
-      <div class="modal-text" v-text="value" />
-    </div>
-  </div>
+	<ol class="modal-container" ref="modal">
+		<li
+			v-for="value in whichModal"
+			:key="value"
+			tag="li"
+			class="modal-list"
+			@click="pickedOrderBy({ value, detail: null })"
+			@mouseover="activatingTab"
+		>
+			<span class="modal-text" v-text="value" />
+		</li>
+	</ol>
 </template>
 
 <script>
 export default {
-  props: ['whichModal'],
-  data() {
-    return {
-      activatedTabIndex: 0,
-    };
-  },
-  methods: {
-    pickedOrderBy(value) {
-      if(this.whichModal.length === 4) {
-        if(this.whichModal.indexOf(value) === 0) {
-          return this.$emit('pickedFirst');
-        }
-        this.$emit('pickedGender', value);
-      } else {
-        this.$emit('pickedOrderBy', value.value);
-      }
-    },
-    activatingTab() {
-      if(!this.$refs.modal) return;
-      this.$refs.modal.childNodes[this.activatedTabIndex].classList.remove('active');
-    },
-    initializingTab() {
-      this.$refs.modal.firstChild.classList.add('active');
-      if(this.whichModal.length > 4) {
-        this.$refs.modal.setAttribute('style', 'width: 150px');
-      } else {
-        this.$refs.modal.setAttribute('style', 'width: 72px');
-      }
-    }
-  },
-  mounted() {
-    this.initializingTab();
-  }
-}
+	props: ['whichModal'],
+
+	data() {
+		return {
+			activatedTabIndex: 0,
+		};
+	},
+
+	methods: {
+		pickedOrderBy(value) {
+			if (this.whichModal.length === 4) {
+				if (this.whichModal.indexOf(value) === 0) {
+					return this.$emit('pickedFirst');
+				}
+				this.$emit('pickedGender', value);
+			} else {
+				this.$emit('pickedOrderBy', value.value);
+			}
+		},
+
+		activatingTab() {
+			if (!this.$refs.modal) return;
+			this.$refs.modal.childNodes[this.activatedTabIndex].classList.remove(
+				'active',
+			);
+		},
+
+		initializingTab() {
+			this.$refs.modal.firstChild.classList.add('active');
+			if (this.whichModal.length > 4) {
+				this.$refs.modal.setAttribute('style', 'width: 150px');
+			} else {
+				this.$refs.modal.setAttribute('style', 'width: 72px');
+			}
+		},
+	},
+
+	mounted() {
+		this.initializingTab();
+	},
+};
 </script>
 
 <style scoped>
 .modal-container {
-  position: absolute;
-  top: 60px;
-  z-index: 9000;
-  color: #000;
-  background-color: #fff;
-  border-radius: 5px;
-  box-shadow: 0 4px 9px 0 rgba(0, 0, 0, 0.16);
+	position: absolute;
+	top: 60px;
+	z-index: 9000;
+	color: #000;
+	background-color: #fff;
+	border-radius: 5px;
+	box-shadow: 0 4px 9px 0 rgba(0, 0, 0, 0.16);
 }
+
 .modal-list {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 40px;
-  cursor: pointer;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 40px;
+	cursor: pointer;
 }
+
 .active {
-  color: #fff;
-  background-color: #42b883;
+	color: #fff;
+	background-color: #42b883;
 }
+
 .modal-list:hover {
-  color: #fff;
-  background-color: #42b883;
+	color: #fff;
+	background-color: #42b883;
 }
+
 .modal-list:first-child {
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
+	border-top-left-radius: 5px;
+	border-top-right-radius: 5px;
 }
+
 .modal-list:last-child {
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
+	border-bottom-left-radius: 5px;
+	border-bottom-right-radius: 5px;
 }
 </style>
