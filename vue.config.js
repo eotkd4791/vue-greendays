914481b6c13c697696cd079f019d74c4ee4aace0
eotkd4791 <eotkd4791@gmail.com>
2021-11-21 20:45:27 +0900

    Add public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	lintOnSave: false,
 	transpileDependencies: ['vuetify'],
-	publicPath: '/'
-	// publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/vue-greendays/' : '/'
 };
